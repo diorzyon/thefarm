7b830b26cb642ee60fe8d3f318dd96ef6a995dab
diorzyon <108488364+diorzyon@users.noreply.github.com>
2023-01-21 22:30:42 +0100

    second commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Product
 
-# Create your views here.
+def index(request):
+    return render(request, "api/index.html")
+
+
+def upload(request):
+    if request.method == "POST": 
+        date = request.POST['date_listed']
+        image =request.FILES['image']
+        products = Product.objects.create(title=request.POST['title'], description=request.POST['description'], image=image, date=date)
+        products.save()
+        
+        return render(request, "api/index.html")
+    else:
+        return render(request, 'api/index.html')
